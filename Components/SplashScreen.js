9801978f09f06ccfe46d071aecb578b2f2ff1f9f
TimW010 <0981877@hr.nl>
2022-05-10 14:25:37 +0200

    Components
    Added a Splashscreen and a Homescreen component.

---
+++ Components/SplashScreen.js
@@ -0,0 +1,97 @@
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { Animated, View, Image, Text, Dimensions } from "react-native-web";
+import React, { useEffect, useRef } from "react";
+import Logo from "../assets/logo.png";
+const BGColor = "#ffffff";
+export default function SplashScreen() {
+  const edges = useSafeAreaInsets();
+  const scaleLogo = useRef(new Animated.Value(1)).current;
+  const startAnimation = useRef(new Animated.Value(0)).current;
+
+  const moveLogo = useRef(new Animated.ValueXY({ x: 0, y: 0 })).current;
+  const contentTransition = useRef(
+    new Animated.Value(Dimensions.get("window").height)
+  ).current;
+
+  useEffect(() => {
+    setTimeout(() => {
+      Animated.parallel([
+        Animated.timing(startAnimation, {
+          toValue: -Dimensions.get("window").height + (edges.top + 65),
+          useNativeDriver: true,
+        }),
+        Animated.timing(scaleLogo, {
+          toValue: 0.3,
+          useNativeDriver: true,
+        }),
+        Animated.timing(moveLogo, {
+          toValue: {
+            x: -Dimensions.get("window").width / 2 - 35,
+            y: -Dimensions.get("window").height / 2 - 5,
+          },
+          useNativeDriver: true,
+        }),
+        Animated.timing(contentTransition, {
+          toValue: 0,
+          useNativeDriver: true,
+        }),
+      ]).start();
+    }, 500);
+  });
+  return (
+    <View
+      style={{
+        position: "absolute",
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+      }}
+    >
+      <Animated.View
+        style={{
+          position: "absolute",
+          top: 0,
+          bottom: 0,
+          left: 0,
+          right: 0,
+          backgroundColor: BGColor,
+          transform: [{ translateY: startAnimation }],
+        }}
+      >
+        <Animated.View
+          style={{
+            flex: 1,
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Animated.Image
+            source={Logo}
+            style={{
+              width: 304,
+              height: 106.86,
+              transform: [
+                { translateX: moveLogo.x },
+                { translateY: moveLogo.y },
+                { scale: scaleLogo },
+              ],
+            }}
+          ></Animated.Image>
+        </Animated.View>
+      </Animated.View>
+      <Animated.View
+        style={{
+          position: "absolute",
+          top: 0,
+          bottom: 0,
+          left: 0,
+          right: 0,
+          backgroundColor: BGColor,
+          zIndex: 0,
+          transform: [{ translateY: contentTransition }],
+        }}
+      ></Animated.View>
+    </View>
+  );
+}
